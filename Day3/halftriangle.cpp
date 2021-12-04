eeced5dc3da4c664b46274020adbed13c5568e87
Lal-Jr <harishlal80@gmail.com>
2021-12-05 01:38:22 +0530

    pattern print

---
+++ Day3/halftriangle.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void pattern(int n)
+{
+    if (n == 0)
+        return;
+    pattern(n - 1);
+    for (int i = 1; i <= n; i++)
+        cout << i << " ";
+    cout << endl;
+}
+
+int main()
+{
+    pattern(5);
+}
