781a6df4c6c693edd061baf61078a93fae256525
Lal-Jr <harishlal80@gmail.com>
2021-12-05 01:02:37 +0530

    sum of n numbers

--- Day3/recursionbasics.cpp
+++ Day3/recursionbasics.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 
-void sayhello()
+// very inmportant: base case
+// other infinite loop and never stops
+
+//trust your function --\> recursive leap of faith
+void sayhello(int n)
 {
-    cout << "hello" << endl;
-    sayhello();
+    while (n == 0) //Stopping case or base case
+    {
+        return;
+    }
+    sayhello(n - 1);
+    cout << "hello" << n << endl;
 }
 
 int main()
 {
-    sayhello();
+    sayhello(10);
 }
