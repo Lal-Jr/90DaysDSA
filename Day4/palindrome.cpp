9c6c8f810313a6d459bb2d084a6e5e7c0880f468
Lal-Jr <harishlal80@gmail.com>
2022-02-24 23:53:56 +0530

    palindrome new

--- Day4/palindrome.cpp
+++ Day4/palindrome.cpp
@@ -2,11 +2,25 @@
 
 using namespace std;
 
-void palindrome()
+bool palindrome(int i, int n, string s[])
 {
+    if (i >= n / 2)
+        return;
+    if (s[i] != s[n - i - 1])
+        return false;
+    return palindrome(i + 1, n - 1, s);
 }
 
 int main()
 {
+    int n;
+    cin >> n;
+    string arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    if (palindrome(0, n, arr) == false)
+        cout << "not palindrome";
+    else
+        cout << "palindrome";
     return 0;
 }
