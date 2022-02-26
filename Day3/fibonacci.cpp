1fe79144812f7e73c4aa0051dcb6f31f6b1f6d6d
Lal-Jr <harishlal80@gmail.com>
2022-02-26 13:01:16 +0530

    fibonacci revised

--- Day3/fibonacci.cpp
+++ Day3/fibonacci.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-//1,2,3,4,5,6,7,8,9,.....
-//0,1,1,2,3,5,8,13,21,....
+// 1,2,3,4,5,6,7,8,9,.....
+// 0,1,1,2,3,5,8,13,21,....
 
-//fib(n) = fib(n-1) + fib(n-2)
+// fib(n) = fib(n-1) + fib(n-2)
 
-int fib(int n){
-    if(n==2){
-        return 1;
-    }
-    if(n==1){
-        return 0;
-    }
-    return fib(n-1) + fib(n-2);
+// time complexity is exponential in nature i.e nearly equal to 2^n as each guy is calling 2 other guys
+
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    cout<<fib(n);
+    cin >> n;
+    cout << fib(n);
 }
