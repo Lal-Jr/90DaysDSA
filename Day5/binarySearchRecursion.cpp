81bf44db4ef11b57670ef61a8fde96451801c3b7
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-06-18 13:25:59 +0530

    Update binarySearchRecursion.cpp

--- Day5/binarySearchRecursion.cpp
+++ Day5/binarySearchRecursion.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 bool binarySearch(int arr[],int start,int end,int key){
     if(start>end) return false;
-    int mid = (start + end)/2;
+    int mid = start + (end-start)/2;
     if(arr[mid] == key) return true;
     if(arr[mid] < key) return binarySearch(arr,mid+1,end,key);
     else return binarySearch(arr,start,mid-1,key);
