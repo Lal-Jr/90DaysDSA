22469fb27555a25540846443a26f6211490590fc
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-06-18 11:06:01 +0530

    error fixed

--- Day5/binarySearchRecursion.cpp
+++ Day5/binarySearchRecursion.cpp
@@ -6,7 +6,7 @@ bool binarySearch(int arr[],int start,int end,int key){
     if(start>end) return false;
     int mid = start + (end-start)/2;
     if(arr[mid] == key) return true;
-    if(arr[mid] > key) return binarySearch(arr,mid+1,end,key);
+    if(arr[mid] < key) return binarySearch(arr,mid+1,end,key);
     else return binarySearch(arr,start,mid-1,key);
 }
 
