5e704fd667a48feb812e1bb811e4c48380cf067f
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-06-18 12:51:45 +0530

    code optimization

--- Day5/binarySearchRecursion.cpp
+++ Day5/binarySearchRecursion.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 bool binarySearch(int arr[],int start,int end,int key){
     if(start>end) return false;
-    int mid = start + (end-start)/2;
+    int mid = (start + end)/2;
     if(arr[mid] == key) return true;
     if(arr[mid] < key) return binarySearch(arr,mid+1,end,key);
     else return binarySearch(arr,start,mid-1,key);
@@ -15,7 +15,7 @@ int main(){
     cin>>size;
     for(int i=0;i<size;i++) cin>>arr[i];
     cin>>key;
-    int start=0, end=size;
+    int start=0, end=size-1;
     if(binarySearch(arr,start,end,key)) cout<<"Key Found";
     else cout<<"Key Not Found";
 }
