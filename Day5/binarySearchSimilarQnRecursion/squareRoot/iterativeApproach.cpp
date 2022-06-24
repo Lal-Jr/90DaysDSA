d718aec8bf1642158b3ccaa4f9ee3fb98cd0271e
Lal-Jr <harishlal80@gmail.com>
2022-06-24 09:31:50 +0530

    sqrt without floatint points

---
+++ Day5/binarySearchSimilarQnRecursion/squareRoot/iterativeApproach.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int sqrt(int x)
+{
+    int start = 0, end = x;
+    long long int mid = start + (end - start) / 2;
+    long long int ans = -1;
+    while (start < end)
+    {
+        long long int square = mid * mid;
+        if (square == x)
+            return mid;
+        if (square < x)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else
+            end = mid - 1;
+        mid = start + (end - start) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sqrt(n);
+}
