c56d06850ca37fdbac97ddc14dea62f007ac9e30
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-03 11:41:19 +0530

    naming changes

--- Day11/frogJump_kJumps_optimizedSolution.cpp
+++ Day11/frogJump_kJumps_optimizedSolution.cpp
@@ -2,29 +2,29 @@
 
 using namespace std;
 
-bool jump(vector<int>& heights){
-    int n = heights.size();
+bool jump(vector<int>& stones){
+    int n = stones.size();
     vector<vector<bool>> dp(n,vector<bool>(n+1,false));
     dp[0][0] = true;
-
-    for(int i=0;i<=n;i++){
+    
+    for(int i=1;i<n;i++){
         for(int j=i-1;j>=0;j--){
-            int k = heights[i] - heights[j];
-            if(j+1<k) break;
+            int k=stones[i] -stones[j];
+            if(k > j+1) break;
             dp[i][k] = dp[j][k-1] or dp[j][k] or dp[j][k+1];
-            if(i==n-1 and  dp[i][k]) return true;
+            if(i==n-1 and dp[i][k]) return true;
         }
     }
     return false;
 }
 
 int main(){
-    vector<int> heights;
+    vector<int> stones;
     int index,x=0;
     cin>>index;
     for(int i=0;i<index;i++){
         cin>>x;
-        heights.push_back(x);
+        stones.push_back(x);
     }
-    cout<<jump(heights);
+    cout<<jump(stones);
 }
