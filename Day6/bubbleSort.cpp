a5fabc7eee810075411e84915d6121e0878b68a8
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-05 15:52:46 +0530

    optimised approach

--- Day6/bubbleSort.cpp
+++ Day6/bubbleSort.cpp
@@ -5,10 +5,14 @@ using namespace std;
 void bubbleSort(vector<int>& arr){
     int n=arr.size();
     for(int i=1;i<n;i++){
+        bool swapped = false;
         for(int j=0;j<n-i;j++){
-            if(arr[j] > arr[j+1])
+            if(arr[j] > arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swapped = true;
+            }
         }
+        if (swapped == false) break;
     }
 }
 
