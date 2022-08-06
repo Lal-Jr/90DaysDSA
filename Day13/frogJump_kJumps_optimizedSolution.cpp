953b2c1918b28ce65f2e35515cae235d91fc2cec
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-07 02:00:51 +0530

    DP date changed

---
+++ Day13/frogJump_kJumps_optimizedSolution.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool jump(vector<int>& stones){
+    int n = stones.size();
+    vector<vector<bool>> dp(n,vector<bool>(n+1,false));
+    dp[0][0] = true;
+    
+    for(int i=1;i<n;i++){
+        for(int j=i-1;j>=0;j--){
+            int k=stones[i] -stones[j];
+            if(k > j+1) break;
+            dp[i][k] = dp[j][k-1] or dp[j][k] or dp[j][k+1];
+            if(i==n-1 and dp[i][k]) return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    vector<int> stones;
+    int index,x=0;
+    cin>>index;
+    for(int i=0;i<index;i++){
+        cin>>x;
+        stones.push_back(x);
+    }
+    cout<<jump(stones);
+}
