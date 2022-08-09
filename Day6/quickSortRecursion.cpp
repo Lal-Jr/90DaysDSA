7d13b470009f4762548a54f9fecce890d6033ec1
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-09 15:35:59 +0530

    quick sort full code using recursion

--- Day6/quickSortRecursion.cpp
+++ Day6/quickSortRecursion.cpp
@@ -3,10 +3,33 @@
 using namespace std;
 
 int partition(vector<int>& arr, int start, int end){
+    int count=0, pivot = arr[start];
+    for(int i=start+1;i<=end;i++){
+        if(pivot>=arr[i])
+            count++;
+    }
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex],arr[start]);
+    
+    int i=start,j=end;
 
+    while(pivotIndex > i && pivotIndex < j){
+        while(arr[pivotIndex] > arr[i]){
+            i++;
+        }
+        while(arr[pivotIndex] < arr[j]){
+            j--;
+        }
+
+        while(arr[i]>arr[pivotIndex] && arr[j]<arr[pivotIndex]){
+            swap(arr[i++],arr[j--]);
+        }
+    }
+
+    return pivotIndex;
 }
 
-void quicksort(vector<int>& arr, int start, int end){
+void quickSort(vector<int>& arr, int start, int end){
     if(start >= end) return;
 
     int p = partition(arr, start, end);
