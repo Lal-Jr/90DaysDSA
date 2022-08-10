79bed6e2a6607ab4404c9a68296e9f84c3a2f90b
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-10 16:14:43 +0530

    permutations of int arr without dup

---
+++ Day7/permutationsWithoutDup.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve(vector<int>& nums,vector<vector<int>>& ans,int index){
+    if(index>=nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+        
+    set<int> placed;
+    for(int j=index;j<nums.size();j++){
+        if(placed.count(nums[j]))
+            continue;
+        placed.insert(nums[j]);
+        swap(nums[index],nums[j]);
+        solve(nums,ans,index+1);
+        swap(nums[j],nums[index]);
+    }
+}
+
+vector<vector<int>> permuteWithoutDup(vector<int>& nums) {
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    solve(nums,ans,0);
+    return ans;
+}
+
+int main(){
+    vector<int> nums;
+    vector<vector<int>> ans;
+    int x,n=3;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        nums.push_back(x);
+    }
+    ans=permuteWithoutDup(nums);
+    for (int i = 0; i < ans.size(); i++){
+        cout<<"[";
+        for (int j = 0; j < ans[i].size(); j++){
+            if(j==(ans[i].size()-1))
+                cout<<ans[i][j];
+            else cout <<ans[i][j]<<",";
+        }
+        if(i==(ans.size()-1))
+            cout<<"]";
+        else cout<<"],";
+    }   
+}
