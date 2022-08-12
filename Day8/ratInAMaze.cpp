38dce19ee1003be2fdb18348f7a6d31a02eccdaa
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-13 01:01:53 +0530

    code refactored

--- Day8/ratInAMaze.cpp
+++ Day8/ratInAMaze.cpp
@@ -2,66 +2,46 @@
 
 using namespace std;
 
-bool isSafe(int x, int y, int n, vector<vector<int>> visited, vector<vector<int>>& m){
-    if((x>=0 && x<n) && (y>=0 && y<n) && visited[x][y] == 0 && m[x][y] == 1){
-        return true;
-    }
-    else return false;
-}
-
-void solve(vector<vector<int>>& m, int n, vector<string>& ans, vector<vector<int>>& visited,int y, int x,string path){
+void solve(vector<vector<int>>& m, int n, vector<string>& ans, vector<vector<int>> visited,int x, int y,string path){
     if((x = n-1) && (y = n-1)) {
         ans.push_back(path);
         return;
     }
     visited[x][y] = 1;
 
-    int newx,newy;
-
-    newx = x+1;
-    newy = y;
-    if(isSafe(newx, newy, n, visited, m)){
+    if(((x+1>=0 && x+1<n) && (y>=0 && y<n) && visited[x][y] == 0 && m[x][y] == 1)){
         path.push_back('D');
-        solve(m,n,ans,visited,newx,newy,path);
+        solve(m,n,ans,visited,x+1, y,path);
         path.pop_back();
     }
 
-    newx = x;
-    newy = y-1;
-    if(isSafe(newx, newy, n, visited, m)){
+    if(((x>=0 && x<n) && (y-1>=0 && y-1<n) && visited[x][y] == 0 && m[x][y] == 1)){
         path.push_back('L');
-        solve(m,n,ans,visited,newx,newy,path);
+        solve(m,n,ans,visited,x, y-1,path);
         path.pop_back();
     }
 
-    newx = x;
-    newy = y+1;
-    if(isSafe(newx, newy, n, visited, m)){
+    if(((x>=0 && x<n) && (y+1>=0 && y+1<n) && visited[x][y] == 0 && m[x][y] == 1)){
         path.push_back('R');
-        solve(m,n,ans,visited,newx,newy,path);
+        solve(m,n,ans,visited,x, y+1,path);
         path.pop_back();
     }
 
-    newx = x-1;
-    newy = y;
-    if(isSafe(newx, newy, n, visited, m)){
+    if(((x-1>=0 && x-1<n) && (y>=0 && y<n) && visited[x][y] == 0 && m[x][y] == 1)){
         path.push_back('U');
-        solve(m,n,ans,visited,newx,newy,path);
+        solve(m,n,ans,visited,x-1, y,path);
         path.pop_back();
     }
-
 
     visited[x][y] = 0;
 }
 
-vector<string> findPath(vector<vector<int>>& m, int n){
+vector<string> searchMaze(vector<vector<int>>& arr, int n){
     vector<string> ans;
 
-    if(m[0][0]==0) return ans;
+    if (arr[n - 1][n - 1] == 0 || arr[0][0] == 0) return ans;
 
-    int srcx=0,srcy=0;
-
-    vector<vector<int>> visited = m;
+    vector<vector<int>> visited = arr;
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -69,8 +49,7 @@ vector<string> findPath(vector<vector<int>>& m, int n){
         }
     }
 
-    string path = "";
-    solve(m,n,ans,visited,srcx,srcy,path);
+    solve(arr,n,ans,visited,0,0,"");
     sort(ans.begin(),ans.end());
     return ans;
 }
@@ -78,14 +57,13 @@ vector<string> findPath(vector<vector<int>>& m, int n){
 int main(){
     vector<vector<int>> matrix;
     vector<string> ans;
-    int x;
     for(int i=0;i<4;i++){
-        for(int j=0;j<5;j++){
-            cin>>x;
-            matrix[i].push_back(x);
+        for(int j=0;j<4;j++){
+            cin>>matrix[i][j];
         }
     }
-    ans=findPath(matrix,4);
+
+    ans=searchMaze(matrix,4);
 
     for(int i=0;i<ans.size();i++){
         cout<<ans[i];
