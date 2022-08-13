6c4f21ceee12641dfd392ca93c5fe3cefdd2ab2b
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-14 02:19:29 +0530

    output error fixed

--- Day8/ratInAMaze.cpp
+++ Day8/ratInAMaze.cpp
@@ -2,52 +2,34 @@
 
 using namespace std;
 
-void solve(vector<vector<int>>& m, int n, vector<string>& ans, vector<vector<int>> visited,int x, int y,string path){
-    if((x = n-1) && (y = n-1)) {
+void solve(vector<vector<int>>& m, int n, vector<string>& ans, vector<vector<bool>>& visited,int x, int y,string path){
+    if((x == n-1) && (y == n-1)) {
         ans.push_back(path);
         return;
     }
+
     visited[x][y] = 1;
 
-    if(((x+1>=0 && x+1<n) && (y>=0 && y<n) && visited[x+1][y] == 0 && m[x+1][y] == 1)){
-        path.push_back('D');
-        solve(m,n,ans,visited,x+1, y,path);
-        path.pop_back();
-    }
+    if(((x+1>=0 && x+1<n) && (y>=0 && y<n) && visited[x+1][y] == 0 && m[x+1][y] == 1))
+        solve(m,n,ans,visited,x+1, y,path + "D");
 
-    if(((x>=0 && x<n) && (y-1>=0 && y-1<n) && visited[x][y-1] == 0 && m[x][y-1] == 1)){
-        path.push_back('L');
-        solve(m,n,ans,visited,x, y-1,path);
-        path.pop_back();
-    }
+    if(((x>=0 && x<n) && (y-1>=0 && y-1<n) && visited[x][y-1] == 0 && m[x][y-1] == 1))
+        solve(m,n,ans,visited,x, y-1,path + "L");
 
-    if(((x>=0 && x<n) && (y+1>=0 && y+1<n) && visited[x][y+1] == 0 && m[x][y+1] == 1)){
-        path.push_back('R');
-        solve(m,n,ans,visited,x, y+1,path);
-        path.pop_back();
-    }
+    if(((x>=0 && x<n) && (y+1>=0 && y+1<n) && visited[x][y+1] == 0 && m[x][y+1] == 1))
+        solve(m,n,ans,visited,x, y+1,path + "R");
 
-    if(((x-1>=0 && x-1<n) && (y>=0 && y<n) && visited[x-1][y] == 0 && m[x-1][y] == 1)){
-        path.push_back('U');
-        solve(m,n,ans,visited,x-1, y,path);
-        path.pop_back();
-    }
-
+    if(((x-1>=0 && x-1<n) && (y>=0 && y<n) && visited[x-1][y] == 0 && m[x-1][y] == 1))
+        solve(m,n,ans,visited,x-1, y,path + "U");
+    
     visited[x][y] = 0;
 }
 
 vector<string> searchMaze(vector<vector<int>>& arr, int n){
     vector<string> ans;
+    vector<vector<bool>> visited (n,vector<bool> (n,0));
 
     if (arr[n - 1][n - 1] == 0 || arr[0][0] == 0) return ans;
-
-    vector<vector<int>> visited = arr;
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            visited[i][j]=0;
-        }
-    }
 
     solve(arr,n,ans,visited,0,0,"");
     sort(ans.begin(),ans.end());
