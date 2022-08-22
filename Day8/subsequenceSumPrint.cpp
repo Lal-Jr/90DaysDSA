5836d9b1af65322b73bec18add8d3b7e16b3c7d5
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2022-08-22 23:18:09 +0530

    print subsequence whose sum is equal to given sum

---
+++ Day8/subsequenceSumPrint.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void subsequenceSum(int arr[],int n, int sum, vector<int> &ds,int index, int s){
+    if(index == n){
+        if(s==sum){
+            for(auto it:ds) cout<<it<<" ";
+            cout<<endl;
+        }
+        return;
+    }
+
+    ds.push_back(arr[index]);
+    s += arr[index];
+
+    subsequenceSum(arr,n,sum,ds,index+1,s);
+    ds.pop_back();
+    s -= arr[index];
+
+    subsequenceSum(arr,n,sum,ds,index+1,s);
+}
+
+int main(){
+    int x;
+    cin>>x;
+    int arr[] = {1,2,1}, n=3,sum=2;
+    vector<int> ds;
+    subsequenceSum(arr,n,sum,ds,0,0);
+}
